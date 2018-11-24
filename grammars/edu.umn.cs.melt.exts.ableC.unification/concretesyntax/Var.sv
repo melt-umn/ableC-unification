352549e19bd3c401b08d72e4fbdaaeefac63f8a5
Lucas Kramer <krame505@umn.edu>
2018-11-23 21:26:56 -0600

    Rename init -> allocate

--- grammars/edu.umn.cs.melt.exts.ableC.unification/concretesyntax/Var.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.unification/concretesyntax/Var.sv
@@ -3,5 +3,5 @@ grammar edu:umn:cs:melt:exts:ableC:unification:concretesyntax;
 marking terminal FreeVar_t 'freevar' lexer classes {Ckeyword};
 
 concrete productions top::PrimaryExpr_c
-| FreeVar_t '<' ty::TypeName_c '>' LParen_t init::AssignExpr_c ')'
-  { top.ast = freeVarExpr(ty.ast, init.ast, location=top.location); }
+| FreeVar_t '<' ty::TypeName_c '>' LParen_t allocate::AssignExpr_c ')'
+  { top.ast = freeVarExpr(ty.ast, allocate.ast, location=top.location); }
